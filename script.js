76ececbc307c9d11d6f11c8f380f291bbd6b3669
Prakhar Pratap Singh <prakharpratap97@gmail.com>
2023-01-15 22:38:14 +0530

    changes

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function TrocaCor() {
+    setInterval(function TrocaCorda() {
+        document.querySelector('.corda1').style.background = '#ffffff'
+        document.querySelector('.corda2').style.background = '#999'
+        document.querySelector('.corda3').style.background = '#ffffff'
+        document.querySelector('.corda4').style.background = '#999'
+        document.querySelector('.corda5').style.background = '#ffffff'
+        document.querySelector('.corda6').style.background = '#999'
+    }, 100);
+
+    setInterval(function TrocaCorda() {
+        document.querySelector('.corda1').style.background = '#999'
+        document.querySelector('.corda2').style.background = '#ffffff'
+        document.querySelector('.corda3').style.background = '#999'
+        document.querySelector('.corda4').style.background = '#ffffff'
+        document.querySelector('.corda5').style.background = '#999'
+        document.querySelector('.corda6').style.background = '#ffffff'
+    }, 500);
+}
